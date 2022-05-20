ae76b7cfe42e7b0461dc3c347da8b3de0200f66d
Vlad <loj.jol.st@yandex.ru>
2022-05-20 20:23:59 +0500

    Add homework

---
+++ homework1/homework1/homework1.cpp
@@ -0,0 +1,38 @@
+﻿//Дано два текстовых файла. Выяснить, совпадают ли их строки. Если нет, то вывести
+// несовподающую строку из каждого файла
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a[200];
+    char b[200];
+    FILE* in1;
+    FILE* in2;
+
+    fopen_s(&in1, "1.txt", "r");
+    fopen_s(&in2, "2.txt", "r");
+
+
+    while (!feof(in1) && !feof(in2))
+    {
+        fgets(a, 200, in1);
+        fgets(b, 200, in2);
+
+        cout << "--------";
+        if (strcmp(a, b))
+        {
+            cout << endl;
+            cout << a << endl << b << endl;
+        }
+        cout << "--------";
+    }
+
+    fclose(in1);
+    fclose(in2);
+
+
+    return 0;
+}
